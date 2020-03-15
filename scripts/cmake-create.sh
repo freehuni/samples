06dded154fb283b2759b36a979cdbf01c7992bff
mh.kang <freehuni@hanmail.net>
2020-03-15 19:18:56 +0900

    [scripts] minior fix

--- scripts/cmake-create.sh
+++ scripts/cmake-create.sh
@@ -13,17 +13,16 @@ fi
 CMAKE_FILE="CMakeLists.txt"
 
 echo "#========================================================="	> $CMAKE_FILE
-echo "# 2019/9/12 mh.kang@hanwha.com"					>> $CMAKE_FILE
-echo "# Create CMakeLists.txt for cmake environment"								>> $CMAKE_FILE
+echo "# Create CMakeLists.txt for cmake environment"				>> $CMAKE_FILE
 echo "#========================================================="	>> $CMAKE_FILE
-echo \									>> $CMAKE_FILE
+echo \										>> $CMAKE_FILE
 echo "CMAKE_MINIMUM_REQUIRED(VERSION 2.8)"	>> $CMAKE_FILE
-echo "SET(PROJECT_NAME \"$1\")"	>> $CMAKE_FILE
-echo "PROJECT(\${PROJECT_NAME})"	>> $CMAKE_FILE
-echo \									>> $CMAKE_FILE
+echo "SET(PROJECT_NAME \"$1\")"				>> $CMAKE_FILE
+echo "PROJECT(\${PROJECT_NAME})"			>> $CMAKE_FILE
+echo \										>> $CMAKE_FILE
 echo "ADD_DEFINITIONS( -D_POSIX -D_LARGEFILE64_SOURCE=1 -DSIZE_64BIT_SUPPORT -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64)"	>> $CMAKE_FILE
 
-echo \									>> $CMAKE_FILE
+echo \										>> $CMAKE_FILE
 
 for FILE in `find $(pwd) -name '*.h'` ;
 do
