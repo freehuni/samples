9df5f1eeb5e15eb67fe5f6200cce171128942806
mh.kang <freehuni@hanmail.net>
2020-03-15 19:16:37 +0900

    [scripts] minor fix

--- scripts/qtmake.sh
+++ scripts/qtmake.sh
@@ -5,7 +5,7 @@
 
 if [ $# -ne 2 ]; then
 	echo 'Usage: qtmake.sh <target> <type>'
-	echo 'type=lib, console,...'
+	echo 'type=lib, app'
 	exit
 fi
 
@@ -13,66 +13,66 @@ fi
 QT_PRO=$1."pro"
 
 echo "#========================================================="	> $QT_PRO
-echo "# 2013/7/24 mhkang2@humaxdigital.com"					>> $QT_PRO
-echo "# Qt Creator project file"								>> $QT_PRO
+echo "# 2013/7/24 mhkang2@humaxdigital.com"							>> $QT_PRO
+echo "# Qt Creator project file"									>> $QT_PRO
 echo "#========================================================="	>> $QT_PRO
 
-echo 'QT -= core gui'  			>> $QT_PRO
-echo TARGET = $1					>> $QT_PRO
+echo 'QT -= core gui'  					>> $QT_PRO
+echo TARGET = $1						>> $QT_PRO
 
-if [ $2 = 'console' ]; then
-	echo 'CONFIG += console'		>> $QT_PRO
-	echo 'CONFIG += qt'			>> $QT_PRO
+if [ $2 = 'app' ]; then
+	echo 'CONFIG += console'			>> $QT_PRO
+	echo 'CONFIG += qt'					>> $QT_PRO
 	echo 'TEMPLATE = app'				>> $QT_PRO
 else
-	echo TEMPLATE = $2				>> $QT_PRO
+	echo TEMPLATE = $2					>> $QT_PRO
 fi
 
-echo \									>> $QT_PRO
+echo \										>> $QT_PRO
 
-echo "#QMAKE_CFLAGS += -0s"	>> $QT_PRO
-echo "#QMAKE_CFLAGS += -Wall"	>> $QT_PRO
+echo "#QMAKE_CFLAGS += -0s"					>> $QT_PRO
+echo "#QMAKE_CFLAGS += -Wall"				>> $QT_PRO
 
-echo DEFINES += '_LARGEFILE64_SOURCE=1'	>> $QT_PRO
+echo DEFINES += '_LARGEFILE64_SOURCE=1'		>> $QT_PRO
 echo DEFINES += 'SIZE_64BIT_SUPPORT '		>> $QT_PRO
-echo DEFINES += '_LARGEFILE_SOURCE'		>> $QT_PRO
-echo DEFINES += '_FILE_OFFSET_BITS=64'	>> $QT_PRO
-echo DEFINES += '_POSIX'						>> $QT_PRO
+echo DEFINES += '_LARGEFILE_SOURCE'			>> $QT_PRO
+echo DEFINES += '_FILE_OFFSET_BITS=64'		>> $QT_PRO
+echo DEFINES += '_POSIX'					>> $QT_PRO
 echo "#DEFINES += more..."					>> $QT_PRO
 
-echo \									>> $QT_PRO
+echo \										>> $QT_PRO
 
 for FILE in `find . -name '*.c'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
 for FILE in `find . -name '*.inc'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
 for FILE in `find . -name '*.cpp'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
 for FILE in `find . -name '*.cc'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
 for FILE in `find . -name '*.java'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
 for FILE in `find . -name '*.txt'` ;
 do
-	echo 'SOURCES += $$PWD/'$FILE 		>> $QT_PRO
+	echo 'SOURCES += $$PWD/'$FILE 			>> $QT_PRO
 done
 
-echo \									>> $QT_PRO
+echo \										>> $QT_PRO
 
 for FILE in `find . -name '*.h'` ;
 do
@@ -94,9 +94,9 @@ do
 	echo 'HEADERS += $$PWD/'$FILE			>> $QT_PRO
 done
 
-echo \									>> $QT_PRO
+echo \										>> $QT_PRO
 
-echo 'INCLUDEPATH += $$PWD/..'					>> $QT_PRO
+echo 'INCLUDEPATH += $$PWD/..'				>> $QT_PRO
 
 echo '#LIBS += -lpthread' >> $QT_PRO
 
