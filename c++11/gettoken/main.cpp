648de1cbb119b824773c2384e0e94c8d086a4abb
mh.kang <freehuni@hanmail.net>
2020-05-13 00:40:21 +0900

    Add enhanced getToken API.

--- c++11/gettoken/main.cpp
+++ c++11/gettoken/main.cpp
@@ -20,15 +20,42 @@ vector<string> getToken(const string& data, const char delimiter)
     return result;
 }
 
+vector<string> getToken(string text, const string delimiter)
+{
+    vector<string> result;
+    size_t pos;
+
+    pos = text.find_first_of(delimiter);
+    while(pos != string::npos)
+    {
+        result.push_back(text.substr(0, pos));
+        text.erase(0, pos + 1);
+        pos = text.find_first_of(delimiter);
+    }
+
+    if (!text.empty())
+    {
+        result.push_back(text);
+    }
+
+    return result;
+}
+
 int main()
 {
-    string mac="1:2:3:4:5:6";
+    string mac="1:2:3:4:5:6-AAA";
     vector<string> tokens;
 
     tokens = getToken(mac, ':');
     for(string item: tokens)
     {
-        printf("item:%s\n", item.c_str());
+        printf("item1:%s\n", item.c_str());
+    }
+
+	tokens = getToken(mac, ":-");
+    for(string item: tokens)
+    {
+        printf("item2:%s\n", item.c_str());
     }
 
     return 0;
