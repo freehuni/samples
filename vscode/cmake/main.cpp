04a29c8ea9320ef79cb20a6331e9d2e25764fb89
mh.kang <freehuni@hanmail.net>
2020-05-29 22:42:00 +0900

    getToken sample

--- vscode/cmake/main.cpp
+++ vscode/cmake/main.cpp
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> getToken(string text, const string delimiter)
+{
+    vector<string> result;
+    size_t pos;
+    
+    pos = text.find_first_of(delimiter);
+    while(pos != string::npos)
+    {
+        result.push_back(text.substr(0, pos));        
+        text.erase(0, pos + 1);
+        pos = text.find_first_of(delimiter);
+    }
+
+    if (!text.empty())
+    {
+        result.push_back(text);
+    }
+
+    return result;
+}
 
 int main()
 {
-    printf("hello cmake~!\n");
+    string mac="aa:bbb:dddd:qq-1000:";
+    vector<string> result;
+    
+    result = getTokens(mac, ":-");
+    for(string item: result)
+    {
+        cout << "itme:" << item << endl;
+    }
+    
+
     return 0;
 }
+
